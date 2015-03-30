e20e94b251901648aacf52aabe773014fe419534
Bert Spaan <bert.spaan@gmail.com>
2015-03-30 17:37:02 +0200

    test stub

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+
+// TODO: create tests:
+//   - create fake Elasticsearch and Core Traversal data
+//   - test config file
+//   - test search parameters
+//   - test JSON LD context insertion
+//   - test Elasticsearch query creation
