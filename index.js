f3296cb5aec6fb2fe96240da99292f2cbeb681b0
Jelle Herold <jelle@defekt.nl>
2015-05-06 14:06:33 +0200

    https API examples

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/', function(req, res) {
     version: '0.1.3',
     message: 'Histograph - historical geocoder (alpha version)',
     docs: 'http://histograph.io/docs',
-    examples: exampleUrls.map(function(query) { return 'http://' + apiUri + query; })
+    examples: exampleUrls.map(function(query) { return 'https://' + apiUri + query; })
   });
 });
 
