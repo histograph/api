89bad06a9f4b332c3f2a2a20006bb7e2e48d488f
Bert Spaan <bert.spaan@gmail.com>
2015-08-12 10:22:37 +0200

    removed old test stub

--- test/test.js
+++
@@ -1,8 +0,0 @@
-//var assert = require("assert");
-
-// TODO: create tests:
-//   - create fake Elasticsearch and Core Traversal data
-//   - test config file
-//   - test search parameters
-//   - test JSON LD context insertion
-//   - test Elasticsearch query creation
